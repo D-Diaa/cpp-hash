5403d0603bf701d44038b02c97f668321d9875ce
Diaa <a.diaa@aucegypt.edu>
2024-09-03 17:29:22 +0300

    Update setup.py to allow for conda libs

--- setup.py
+++ setup.py
@@ -1,4 +1,22 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
+import os
 
-setup(name='hash_cpp', ext_modules=[cpp_extension.CUDAExtension('hash_cpp', ['hash.cpp', 'hash_kernel.cu'], libraries=["ssl", "crypto"], library_dirs = ["/usr/lib"])], cmdclass={'build_ext': cpp_extension.BuildExtension})
+# Get the path to the Conda environment
+conda_env_path = os.environ.get('CONDA_PREFIX', '')
+
+# Construct the library directory path
+lib_dir = os.path.join(conda_env_path, 'lib')
+
+setup(
+    name='hash_cpp',
+    ext_modules=[
+        cpp_extension.CUDAExtension(
+            'hash_cpp',
+            ['hash.cpp', 'hash_kernel.cu'],
+            libraries=["ssl", "crypto"],
+            library_dirs=["/usr/lib", lib_dir],
+        )
+    ],
+    cmdclass={'build_ext': cpp_extension.BuildExtension}
+)
